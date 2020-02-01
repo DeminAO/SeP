6ad3ab2bdca7845d3d5a6fa45ed5d78db2fb183b
t89897067604@yandex.ru <t89897067604@yandex.ru>
2020-02-02 00:08:43 +0300

    подготовка mvvm проекта. создано представление для добавления предикатов и фактов. Добавление высказываний происходит без проверки. Перерасчет нвоых фактов не перенесен.

---
+++ Loss/Models/Fact.cs
@@ -0,0 +1,45 @@
+﻿using Loss.Helpers;
+using System.Collections.ObjectModel;
+
+namespace Loss.Models
+{
+	public class Fact : BindableBase
+	{
+		private Models.Predicate parent;
+		private ObservableCollection<string> arguments;
+
+		public Models.Predicate Parent
+		{
+			get => parent;
+			set => SetProperty(ref parent, value);
+		}
+
+		public ObservableCollection<string> Arguments
+		{
+			get
+			{
+				if (arguments == null)
+					arguments = new ObservableCollection<string>();
+
+				return arguments;
+			}
+			set
+			{
+				SetProperty(ref arguments, value);
+				SetProperty(nameof(StringArguments));
+			}
+		}
+
+		private string stringArguments;
+		public string StringArguments
+		{
+			get => string.Join(", ", Arguments);
+			set
+			{
+				SetProperty(ref stringArguments, value);
+				var args = stringArguments.Split(new string[] { ", ", "," }, System.StringSplitOptions.RemoveEmptyEntries);
+				Arguments = new ObservableCollection<string>(args);
+			}
+		}
+	}
+}
---
+++ Loss/Models/Pedicate.cs
@@ -0,0 +1,22 @@
+﻿using Loss.Helpers;
+
+namespace Loss.Models
+{
+	public class Predicate : BindableBase
+	{
+		private string name;
+		private int? argumentsCount;
+
+		public string Name
+		{
+			get => name;
+			set => SetProperty(ref name, value);
+		}
+
+		public int? ArgumentsCount
+		{
+			get => argumentsCount;
+			set => SetProperty(ref argumentsCount, value);
+		}
+	}
+}
