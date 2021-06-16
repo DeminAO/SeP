00f7b740b22e2c76cbfcf9fcbdc8b041cc8a3e36
DeminAORostov <57270583+DeminAO@users.noreply.github.com>
2021-06-16 03:08:51 +0300

    * template

--- Integrazie.Client/Integrazie.Client/Messeging/ViewModels/DialogsViewPageViewModel.cs
+++ Integrazie.Client/Integrazie.Client/Messeging/ViewModels/DialogsViewPageViewModel.cs
@@ -5,8 +5,27 @@ using System.Text;
 
 namespace CrossMessenger.Client.Messeging.ViewModels
 {
+	public class Contact
+	{
+		public List<string> Images { get; set; }
+		public string Name { get; set; }
+		public string LastMessage { get; set; }
+	}
+
 	public class DialogsViewPageViewModel : BindableBase
 	{
+		public List<Contact> Contacts { get; private set; }
 
+		public DialogsViewPageViewModel()
+		{
+			Contacts = new List<Contact>
+			{
+				new Contact { Images =new List<string> { "tglogo.png", "vklogo.png" }, LastMessage = "Давай", Name = "Влад Иванов" },
+				new Contact { Images =new List<string> { "vklogo.png" }, LastMessage = "Да так же", Name = "Юля Ростовская" },
+				new Contact { Images =new List<string> { "vklogo.png" }, LastMessage = "понимаю", Name = "Даниил богданов" },
+				new Contact { Images =new List<string> { "tglogo.png" }, LastMessage = "Ну ок)", Name = "Евгений Гурченко" },
+				new Contact { Images =new List<string> { "vklogo.png" }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+			};
+		}
 	}
 }
