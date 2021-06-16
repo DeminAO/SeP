4b409dc8ed8f3f70ebe4d5d55c692487d7c43a85
DeminAORostov <57270583+DeminAO@users.noreply.github.com>
2021-06-16 03:47:16 +0300

    * fill collection

--- Integrazie.Client/Integrazie.Client/Messeging/ViewModels/DialogsViewPageViewModel.cs
+++ Integrazie.Client/Integrazie.Client/Messeging/ViewModels/DialogsViewPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using CrossMessenger.Client.Infrastructure.Enums;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,7 +8,7 @@ namespace CrossMessenger.Client.Messeging.ViewModels
 {
 	public class Contact
 	{
-		public List<string> Images { get; set; }
+		public List<MessengersTypes> Images { get; set; }
 		public string Name { get; set; }
 		public string LastMessage { get; set; }
 	}
@@ -20,11 +21,42 @@ namespace CrossMessenger.Client.Messeging.ViewModels
 		{
 			Contacts = new List<Contact>
 			{
-				new Contact { Images =new List<string> { "tglogo.png", "vklogo.png" }, LastMessage = "Давай", Name = "Влад Иванов" },
-				new Contact { Images =new List<string> { "vklogo.png" }, LastMessage = "Да так же", Name = "Юля Ростовская" },
-				new Contact { Images =new List<string> { "vklogo.png" }, LastMessage = "понимаю", Name = "Даниил богданов" },
-				new Contact { Images =new List<string> { "tglogo.png" }, LastMessage = "Ну ок)", Name = "Евгений Гурченко" },
-				new Contact { Images =new List<string> { "vklogo.png" }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Telegram, MessengersTypes.Vk }, LastMessage = "Давай", Name = "Влад Иванов" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Да так же", Name = "Юля Ростовская" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "понимаю", Name = "Даниил богданов" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Евгений Гурченко" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
+				new Contact { Images =new List<MessengersTypes> { MessengersTypes.Vk }, LastMessage = "Ну ок)", Name = "Олег Демин" },
 			};
 		}
 	}
